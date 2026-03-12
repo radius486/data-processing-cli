8a387e9880f4ba864744adc98f483b58733204fc
Rodion Grishaev <radius486@gmail.com>
2026-03-13 00:53:17 +0300

    feat: cd command implementation.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import path from 'path';
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 import os from 'node:os';
+import fs from 'node:fs/promises';
 
 const __dirname = path.resolve();
 const __homedir = os.homedir();
@@ -10,20 +11,33 @@ const rl = readline.createInterface({ input, output });
 let __currentDir = __homedir;
 
 const dataProcessingToolkit = async () => {
-
   async function prompt() {
-
     console.log('Welcome to Data Processing CLI!');
     console.log(`You are currently in ${__currentDir}`);
 
-    const command = await rl.question('> ');
+    const userInput = await rl.question('> ');
+    const [command, value] = userInput.split(' ');
 
     switch (command) {
       case 'up':
         __currentDir = path.join(__currentDir, '../');
         break;
-      case 'cwd':
-        console.log('Current working directory: ', __currentDir);
+      case 'cd':
+        const __newDir = value.startsWith('/') ? value : path.join(__currentDir, value);
+
+        try {
+          const stats = await fs.stat(__newDir);
+          const isDirectory = stats.isDirectory();
+
+          if (!isDirectory) {
+            throw new Error('Not a directory');
+          }
+
+          __currentDir = __newDir;
+        } catch (err) {
+          console.log('Operation failed');
+        }
+
         break;
       case 'date':
         const date = new Date().toISOString();
